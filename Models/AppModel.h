8fae13bb0cf1490be191a94fd64c6b904b76ab8d
David <dzz0615@gmail.com>
2012-10-12 12:30:39 -0400

    add core data models

--- Models/AppModel.h
+++ Models/AppModel.h
@@ -7,8 +7,16 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
 #import "RoadtripModel.h"
 
 @interface AppModel : NSObject
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
 
 @end
