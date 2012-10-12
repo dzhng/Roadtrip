7bea6fdd4c96b2c05b4c9cda575d692a275652b9
David <dzz0615@gmail.com>
2012-10-12 00:44:17 -0400

    added app model file

---
+++ Models/AppModel.h
@@ -0,0 +1,14 @@
+//
+//  AppModel.h
+//  Roadtrip
+//
+//  Created by David Zhang on 10/12/12.
+//  Copyright (c) 2012 David Zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RoadtripModel.h"
+
+@interface AppModel : NSObject
+
+@end
