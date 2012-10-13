5858f8e4fa2175f814135869a7514152425a1261
Zach Zimbler <zachzimbler@gmail.com>
2012-10-13 15:12:49 -0400

    added a new root view controller with a collection view inside, totally unfinished

---
+++ Controllers and Views/MainViewController.m
@@ -0,0 +1,47 @@
+//
+//  MainViewController.m
+//  Roadtrip
+//
+//  Created by Zachary Zimbler on 10/13/12.
+//  Copyright (c) 2012 David Zhang. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    //login view displayed mmodally
+    PFLogInViewController
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
