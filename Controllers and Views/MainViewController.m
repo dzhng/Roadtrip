76f5668486c6c6f2113f0b2ac8b4e85c8eac1cbf
Zach Zimbler <zachzimbler@gmail.com>
2012-10-13 15:27:37 -0400

    added the basic parse sign in screen

--- Controllers and Views/MainViewController.m
+++ Controllers and Views/MainViewController.m
@@ -34,7 +34,9 @@
     [super viewDidAppear:animated];
     
     //login view displayed mmodally
-    PFLogInViewController
+    PFLogInViewController *login = [[PFLogInViewController alloc] init];
+    [self presentModalViewController:login animated:YES];
+    
     
 }
 
